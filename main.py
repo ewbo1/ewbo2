41fc2ad67a211c9a9ca4efa28a585ae8ed391d0f
ewbo1 <94215084+ewbo1@users.noreply.github.com>
2021-11-21 15:01:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from token import token
+# import telebot
+# bot = telebot.TeleBot(token["TOKEN"])
+
+# @bot.message_handler(commands=["start"])
+# def start_func(message):
+#     bot.send_message(message.chat.id, "Как тебя зовут?")
+
+# @bot.message_handler(content_types=["text"])
+# def hello(message):
+#     msg = bot.send_message(message.chat.id, f"Привет, {message.text}")
+#     # bot.register_next_step_handler(msg.name)
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     bot.polling(none_stop=True)
+
 import telebot
+
 bot = telebot.TeleBot(token["TOKEN"])
 
-@bot.message_hendler(commands=["start"])
+@bot.message_handler(commands=["start"])
 def start_func(message):
     bot.send_message(message.chat.id, "Как тебя зовут?")
 
-@bot.message_hendler(commands=["text"])
+@bot.message_handler(content_types=["text"])
 def hello(message):
     msg = bot.send_message(message.chat.id, f"Привет, {message.text}")
-    # bot.register_next_step_handler(msg.name)
-
-
-
-
-
+    # bot.register_next_step_handler(msg, name)
 
 if __name__ == '__main__':
-    bot.pollimg(non_stop=True)
+    bot.polling(none_stop=True)
