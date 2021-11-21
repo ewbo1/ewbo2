a1e59c70f7ae18442ed5c697de8dfd6d8cd5fe15
ewbo1 <94215084+ewbo1@users.noreply.github.com>
2021-11-21 18:37:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import telebot
 from telebot import types
 
 
-bot = telebot.TeleBot("2100247655:AAHLBo6lZK99bDQQDNlXwTpu7f0glUvzcSA")
+bot = telebot.TeleBot("2112306268:AAHUSJz8bHkM9a7zvgTRcBzYjb0PiCHIH8w")
 print("start")
 
-# button
-boardbtn = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True) #Создаем поле для кнопок
-# Создаём кнопки
-btn1 = types.KeyboardButton("да") 
-btn2 = types.KeyboardButton("нет")
-# Добавляем кнопки в поле
-boardbtn.add(btn1, btn2)
+# # button
+# boardbtn = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True) #Создаем поле для кнопок
+# # Создаём кнопки
+# btn1 = types.KeyboardButton("да") 
+# btn2 = types.KeyboardButton("нет")
+# # Добавляем кнопки в поле
+# boardbtn.add(btn1, btn2)
 
 
 #inline button
@@ -33,28 +33,37 @@ def start_func(message):
 @bot.message_handler(content_types=["text"])
 def hello(message):
     bot.send_message(message.chat.id, f"Привет, {message.text}")
-    msg = bot.send_message(message.chat.id, f"ты сейчас за компьютером?", reply_markup=boardbtn) # reply_markup - добавляет созданную нами клавиатура у с кнопками
-    bot.register_next_step_handler(msg, computer) # Переносит нас в следующую функцию
+    msg = bot.send_message(message.chat.id, f"ты сейчас читаешь какой нибудь фф?", reply_markup=inline_btn) # reply_markup - добавляет созданную нами клавиатура у с кнопками
+    bot.register_next_step_handler(msg, next) # Переносит нас в следующую функцию
 
-@bot.message_handler(content_types=["text"])
-def computer(message):
-    if message.text.lower() == "да": # Проверка на ответ пользователя
-        msg = bot.send_message(message.chat.id, f"Долго не сиди", reply_markup=None)
-        bot.register_next_step_handler(msg, next)
-    elif message.text.lower() == "нет":
-        msg = bot.send_message(message.chat.id, f"в телефоне?", reply_markup=inline_btn) # Прикрепление inline клавиатуры к нашему сообщению
-        bot.register_next_step_handler(msg, next)
-    else:
-        bot.send_message(message.chat.id, f"я тебя не понимаю, введи 'да' или 'нет'")
+# @bot.message_handler(content_types=["text"])
+# def computer(message):
+#     if message.text.lower() == "да": # Проверка на ответ пользователя
+#         msg = bot.send_message(message.chat.id, f"Долго не сиди", reply_markup=inline_btn) #reply_markup=None)
+#         bot.register_next_step_handler(msg, next)
+#     elif message.text.lower() == "нет":
+#         msg = bot.send_message(message.chat.id, f"в телефоне?", reply_markup=inline_btn) # Прикрепление inline клавиатуры к нашему сообщению
+#         bot.register_next_step_handler(msg, next)
+#     else:
+#         bot.send_message(message.chat.id, f"я тебя не понимаю, введи 'да' или 'нет'")
 
 
 # функция ответа на inline кнопку (callback)
 @bot.callback_query_handler(func=lambda call: True)
 def next(call):
     if call.data == "yes":
-        bot.edit_message_text(text="OK", chat_id=call.message.chat.id, message_id=call.message.id) # редактируем сообщение
+        bot.edit_message_text(text="какой?", chat_id=call.message.chat.id, message_id=call.message.id) # редактируем сообщение
     elif call.data == "no":
-        bot.edit_message_text(text="Странно", chat_id=call.message.chat.id, message_id=call.message.id)
+        bot.edit_message_text(text="подсказать?", chat_id=call.message.chat.id, message_id=call.message.id)
+        def computer(message):
+            if message.text.lower() == "да": # Проверка на ответ пользователя
+                msg = bot.send_message(message.chat.id, f"Незаменимая", reply_markup=inline_btn) #reply_markup=None)
+                bot.register_next_step_handler(msg, next1)
+            elif message.text.lower() == "нет":
+                msg = bot.send_message(message.chat.id, f"ок", reply_markup=inline_btn) # Прикрепление inline клавиатуры к нашему сообщению
+                bot.register_next_step_handler(msg, next)
+            else:
+                bot.send_message(message.chat.id, f"я тебя не понимаю, введи 'да' или 'нет'")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
